d7b831cdc5a09be9467786cc41230de96ab9e03b
kyhfan <yh.kim@minivertising.kr>
2015-05-11 18:47:54 +0900

    리다이렉트 로직

--- PC/redirect.php
+++ PC/redirect.php
@@ -1,3 +1,11 @@
 <?
 	include_once "../config.php";
+
+	$serial		= $_REQUEST['serial'];
+
+	$query 		= "UPDATE ".$_gl['member_info_table']." SET mb_join='Y' WHERE mb_serial='".$serial."'";
+	$result 	= mysqli_query($my_db, $query);
+
+	if ($result)
+		echo "<script>location.href='http://plus.kakao.com/home/@vdl';</script>";
 ?>

7fe17624949907e55013b9759492123738cc993e
Seonhye yang <yangseonhye@yangseonhyeui-iMac.local>
2015-05-13 13:57:05 +0900

    index 코딩 완료

---
+++ MOBILE/header.php
@@ -0,0 +1,42 @@
+<?
+	include_once "../config.php";
+?>
+<!doctype html>
+<html>
+  <head>
+    <title>belif bomb</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0" />
+    <meta property="og:title" content="수분폭탄, 즐거움이 터진다">
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="http://www.belifbomb.com/MOBILE/index.php" />
+    <meta property="og:image" content="http://www.belifbomb.com/MOBILE/img/sns_image2.png" />
+    <meta property="og:description" content="지금 정해진 시간안에 폭탄 크림을 터트리면, 빌리프의 다양한 선물이 쏟아집니다. 더 많은 이벤트 자세히 보기">
+
+    <link rel="shortcut icon" type="image/x-icon" href="./images/icon/favicon.ico" />
+    <title>Belif</title>
+    <link rel="stylesheet" type="text/css" href="./css/style.css" />
+    <link rel="stylesheet" href="../lib/iCheck/skins/all.css">
+    <link rel="stylesheet" href="../lib/Magnific-Popup/magnific-popup.css"> 
+	<link href="http://code.jquery.com/ui/1.10.4/themes/ui-lightness/jquery-ui.css" rel="stylesheet">
+    <script type='text/javascript' src="../js/jquery-1.11.2.min.js"></script>
+    <script type="text/javascript" src="../js/jquery.easing.1.3.js"></script>
+	<script src="http://code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
+    <script type='text/javascript' src="../js/analytics.js"></script>
+    <script type='text/javascript' src='../lib/iCheck/icheck.js'></script>
+    <script type="text/javascript" src="http://www.youtube.com/player_api"></script>
+    <script type="text/javascript" src="../lib/Magnific-Popup/jquery.magnific-popup.js"></script>
+    <script type="text/javascript" src="https://developers.kakao.com/sdk/js/kakao.min.js"></script>
+    <script type='text/javascript' src="../js/main.js"></script>
+    <script type="text/javascript" src="../js/jQueryTween.min.js"></script>
+    <script type="text/javascript" src="../js/RequestAnimationFrame.js"></script>
+    <script type="text/javascript" src="../js/tween.min.js"></script>
+    <script src="https://developers.kakao.com/sdk/js/kakao.min.js"></script>
+	<script type='text/javascript' src='../js/analytics.js'></script>
+    <style>
+	html,body { height: 100%; margin: 0; padding: 0;}
+    </style>
+
+  </head>

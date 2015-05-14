1b97ab1cc8de4598187941c12b00698a4dec2700
kyhfan <yh.kim@minivertising.kr>
2015-05-14 15:03:38 +0900

    모바일 메세지 페이지

--- MOBILE/message.php
+++ MOBILE/message.php
@@ -1,5 +1,5 @@
 <?
-	include_once   "./header.php";
+	//include_once   "./header.php";
 
 	$serial		= $_REQUEST['serial'];
 
@@ -14,28 +14,65 @@
 	//if ($result)
 		//echo "<script>location.href='http://plus.kakao.com/home/@vdl';</script>";
 ?>
-    <div class="block_mms_received">
-      <div class="mms">
-        <div class="inner_mms">
-          <div class="title_to clearfix">
-            <div class="label">TO.</div>
-            <div class="txt"><?=$member_info['mb_receive']?></div>
-          </div>
-          <div class="title_mss">
-          <?=$member_info['mb_message']?>
-          </div>
-          <div class="title_from">
-            <div class="label">FROM.</div>
-            <div class="txt"><?=$member_info['mb_send']?></div>
-          </div>
+<?
+	include_once "../config.php";
+?>
+<!doctype html>
+<html>
+  <head>
+    <title>VDL KAKAO FRIENDS</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0" />
+    <meta property="og:title" content="VDL MEETS KAKAO FRIENDS">
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="http://www.mnv.kr/MOBILE/index.php" />
+    <meta property="og:image" content="http://www.mnv.kr/MOBILE/images/img_sns_share.jpg" />
+    <meta property="og:description" content="재기 발랄한 카카오 프렌즈와 감각적인 브이디엘이 만나 선보이는 키치 꾸뛰르 감성의 써머 컬렉션">
+
+    <link rel="shortcut icon" type="image/x-icon" href="./images/favicon.ico" />
+    <link rel="stylesheet" type="text/css" href="./css/style.css" />
+    <link rel="stylesheet" href="../lib/iCheck/skins/all.css">
+    <link rel="stylesheet" href="../lib/Magnific-Popup/magnific-popup.css"> 
+	<link href="http://code.jquery.com/ui/1.10.4/themes/ui-lightness/jquery-ui.css" rel="stylesheet">
+    <script type='text/javascript' src="../js/jquery-1.11.2.min.js"></script>
+    <script type="text/javascript" src="../js/jquery.easing.1.3.js"></script>
+	<script src="http://code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
+    <script type='text/javascript' src="../js/analytics.js"></script>
+    <script type='text/javascript' src='../lib/iCheck/icheck.js'></script>
+    <script type="text/javascript" src="http://www.youtube.com/player_api"></script>
+    <script type="text/javascript" src="../lib/Magnific-Popup/jquery.magnific-popup.js"></script>
+    <script type="text/javascript" src="https://developers.kakao.com/sdk/js/kakao.min.js"></script>
+    <script type='text/javascript' src="../js/main.js"></script>
+    <script src="https://developers.kakao.com/sdk/js/kakao.min.js"></script>
+    <style>
+	html,body { height: 100%; margin: 0; padding: 0;}
+    </style>
+
+  </head>
+
+<body class="bg_mms">
+	<div class="block_mms_received">
+    	<div class="mms">
+        	<div class="inner_mms">
+                <div class="title_to clearfix">
+                	<div class="label">TO.</div>
+                	<div class="txt"><?=$member_info['mb_receive']?></div>
+                </div>
+                <div class="title_mss">
+			    <?=$member_info['mb_message']?>
+                </div>
+                <div class="title_from">
+                	<div class="label">FROM.</div>
+                	<div class="txt"><?=$member_info['mb_send']?></div>
+                </div>
+            </div>
         </div>
-      </div>
-      <div class="btn_block"><a href="#" onclick="go_friends('<?=$serial	?>')"><img src="images/btn_plus_go.png" /></a></div>
-      <div class="img_mms_bg"><img src="images/bg_mss.jpg" /></div>
+        <div class="btn_block"><a href="#" onclick="go_friends('<?=$serial	?>')"><img src="images/btn_plus_go.png" /></a></div>
+    	<div class="img_mms_bg"><img src="images/bg_mss.jpg" /></div>
     </div>
   </body>
 </html>
-
-
 
 

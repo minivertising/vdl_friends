062feede495c44e6a8f47032d91dbc60368306be
kyhfan <yh.kim@minivertising.kr>
2015-05-19 12:09:59 +0900

    11

--- MOBILE/winner_coupon.php
+++ MOBILE/winner_coupon.php
@@ -27,8 +27,6 @@
 <html>
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=3.0" />
     <meta property="og:title" content="VDL MEETS KAKAO FRIENDS">
     <meta property="og:type" content="website" />
@@ -38,22 +36,8 @@
 
     <link rel="shortcut icon" type="image/x-icon" href="./images/favicon.ico" />
     <link rel="stylesheet" type="text/css" href="./css/style.css" />
-    <link rel="stylesheet" href="../lib/iCheck/skins/all.css">
-    <link rel="stylesheet" href="../lib/Magnific-Popup/magnific-popup.css"> 
-	<link href="http://code.jquery.com/ui/1.10.4/themes/ui-lightness/jquery-ui.css" rel="stylesheet">
-    <script type='text/javascript' src="../js/jquery-1.11.2.min.js"></script>
-    <script type="text/javascript" src="../js/jquery.easing.1.3.js"></script>
-	<script src="http://code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
     <script type='text/javascript' src="../js/analytics.js"></script>
-    <script type='text/javascript' src='../lib/iCheck/icheck.js'></script>
-    <script type="text/javascript" src="http://www.youtube.com/player_api"></script>
-    <script type="text/javascript" src="../lib/Magnific-Popup/jquery.magnific-popup.js"></script>
-    <script type="text/javascript" src="https://developers.kakao.com/sdk/js/kakao.min.js"></script>
     <script type='text/javascript' src="../js/main.js"></script>
-    <script src="https://developers.kakao.com/sdk/js/kakao.min.js"></script>
-    <style>
-	html,body { height: 100%; margin: 0; padding: 0;}
-    </style>
 
   </head>
 
